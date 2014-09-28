9918886981d59a4c188bed7bbd2c7c5a2bc78c7b
Scott Carr <s.carr1024@gmail.com>
2014-09-28 11:56:14 -0400

    move github cloning tool under reviewbot project

---
+++ Github/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net;
+using System.IO;
+using System.Diagnostics;
+namespace Github
+{
+    class Program
+    {
+        static readonly string gitCmd = @"C:\Users\Scott\AppData\Local\GitHub\PortableGit_69703d1db91577f4c666e767a6ca5ec50a48d243\bin\git.exe";
+        static void Main(string[] args)
+        {
+            const int N = 1; // how many hundreds of results you want
+            var repos = new List<Tuple<string, string>>(); // for now I am just keeping the name and clone url (both strings)
+
+            for (int i = 0; i < N; i++)
+            {
+                var urlWithHole = "https://api.github.com/search/repositories?q=language:csharp&sort=stars&order=desc&page={0}&per_page=100/";
+                var url = String.Format(urlWithHole, i);
+            	var request = HttpWebRequest.Create(new Uri(url)) as HttpWebRequest;
+            	request.UserAgent = "ReviewBot"; // YOU MUST HAVE THIS SET TO SOMETHING !!!!
+            	var resp = request.GetResponse() as HttpWebResponse;
+            	var reader = new StreamReader(resp.GetResponseStream());
+            	var results = JsonConvert.DeserializeObject<SearchResponse>(reader.ReadToEnd());
+                foreach (var result in results.items) 
+                {
+                    repos.Add(new Tuple<string,string>(result.name, result.clone_url));
+                    cloneRepo(result.clone_url);
+                }
+            }
+            repos.ForEach(Console.WriteLine);
+            Console.ReadKey();
+        }
+        static void cloneRepo(string url)
+        {
+            Process p = new Process();
+            p.StartInfo.FileName = gitCmd;
+            p.StartInfo.Arguments = String.Format("clone {0}", url);
+            p.StartInfo.UseShellExecute = true;
+            p.Start();
+            p.WaitForExit();
+        }
+    }
+} 
---
+++ Github/SearchResponse.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Github
+{
+    class SearchResponse
+    {
+        public string total_count;
+        public string incomplete_results;
+        public Result[] items;
+    }
+    class Result
+    {
+        public string id;
+        public string name;
+        public string full_name;
+        public Owner owner;
+        public bool isPrivate;
+        public string html_url;
+      	public string description;
+      	public string fork;
+      	public string url;
+      	public string forks_url;
+      	public string keys_url;
+      	public string collaborators_url;
+      	public string teams_url;
+      	public string hooks_url;
+      	public string issue_events_url;
+      	public string events_url;
+      	public string assignees_url;
+      	public string branches_url;
+      	public string tags_url;
+      	public string blobs_url;
+      	public string git_tags_url;
+      	public string git_refs_url;
+      	public string trees_url;
+      	public string statuses_url;
+      	public string languages_url;
+      	public string stargazers_url;
+      	public string contributors_url;
+      	public string subscribers_url;
+      	public string subscription_url;
+      	public string commits_url;
+      	public string git_commits_url;
+      	public string comments_url;
+      	public string issue_comment_url;
+      	public string contents_url;
+      	public string compare_url;
+      	public string merges_url;
+      	public string archive_url;
+      	public string downloads_url;
+      	public string issues_url;
+      	public string pulls_url;
+      	public string milestones_url;
+      	public string notifications_url;
+      	public string labels_url;
+      	public string releases_url;
+      	public string created_at;
+      	public string updated_at;
+      	public string pushed_at;
+      	public string git_url;
+      	public string ssh_url;
+      	public string clone_url;
+      	public string svn_url;
+      	public string homepage;
+      	public string size;
+      	public string stargazers_count;
+      	public string watchers_count;
+      	public string language;
+      	public string has_issues;
+      	public string has_downloads;
+      	public string has_wiki;
+      	public string forks_count;
+      	public string mirror_url;
+      	public string open_issues_count;
+      	public string forks;
+      	public string open_issues;
+      	public string watchers;
+      	public string default_branch;
+      	public string score;
+
+    }
+    class Owner
+    {
+        public string login;
+        public string id;
+        public string avatar_url;
+        public string gravatar_id;
+        public string url;
+        public string html_url;
+        public string followers_url;
+        public string following_url;
+        public string gists_url;
+        public string starred_url;
+        public string subscriptions_url;
+        public string organizations_url;
+        public string repos_url;
+        public string events_url;
+        public string received_events_url;
+        public string type;
+        public string site_admin;
+    }
+}
