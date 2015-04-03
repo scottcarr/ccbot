0343862d83f2834999751c545db77ed98e616398
Scott Carr <s.carr1024@gmail.com>
2015-04-03 10:02:51 -0400

    checkin before I start deleting new stuff

--- Github/GitScanner.cs
+++ Github/GitScanner.cs
@@ -70,6 +70,105 @@ namespace Microsoft.Research.ReviewBot.Github
             @"monodevelop\main\Main.sln"
 
     };
+    static IEnumerable<MetadataReference> GetFacadeReferences(string version)
+    {
+      var refs = new List<MetadataReference>();
+      var facadesDir = String.Format(@"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\{0}\Facades\", version);
+      string[] dlls =
+      {
+       "System.Collections.dll",
+       "System.Collections.Concurrent.dll",
+       "System.Globalization.dll",
+       "System.IO.dll",
+       "System.Reflection.dll",
+       "System.Reflection.Extensions.dll",
+       "System.Reflection.Primitives.dll",
+       "System.Resources.ResourceManager.dll",
+       "System.Runtime.dll",
+       "System.Runtime.Extensions.dll",
+       "System.Runtime.InteropServices.dll",
+       "System.Text.Encoding.dll",
+       "System.Threading.dll",
+       "System.Threading.Tasks.dll",
+       "System.Xml.ReaderWriter.dll",
+      };
+      var dllPaths = dlls.Select(x => facadesDir + x);
+      refs.AddRange(dllPaths.Select(f => MetadataReference.CreateFromFile(f)));
+      return refs;
+    }
+    static MetadataReference GetMSCorLibRef()
+    {
+      return MetadataReference.CreateFromAssembly(typeof(object).Assembly);
+    }
+    static ReferenceStatus FindIfMissingReferences(Project proj, out List<string> why)
+    {
+      List<string> orig_msg, mscl_msg, facade45_msg, facade46_msg;
+      why = new List<string>();
+      if (!HasErrors(proj, out orig_msg))
+      {
+        return ReferenceStatus.OK;
+      }
+      var projWithMSCL = proj.AddMetadataReference(GetMSCorLibRef());
+      if (!HasErrors(projWithMSCL, out mscl_msg))
+      {
+        return ReferenceStatus.MissingMSCorLib;
+      }
+      var projWithFacades45 = proj.AddMetadataReferences(GetFacadeReferences("v4.5"));
+      if (!HasErrors(projWithFacades45, out facade45_msg))
+      {
+        return ReferenceStatus.MissingFacades45;
+      }
+      var projWithFacades46 = proj.AddMetadataReferences(GetFacadeReferences("v4.6"));
+      if (!HasErrors(projWithFacades46, out facade46_msg))
+      {
+        return ReferenceStatus.MissingFacades46;
+      }
+
+      List<string>[] msgs = { orig_msg, mscl_msg, facade45_msg, facade46_msg };
+      var lens = msgs.Select(x => x.Count());
+      var shortest_len = lens.Min();
+      var shortest = msgs.First(x => x.Count() == shortest_len);
+      /*
+      if (shortest == orig_msg) { Console.WriteLine("orig: "); }
+      if (shortest == mscl_msg) { Console.WriteLine("mscl: "); }
+      if (shortest == facade45_msg) { Console.WriteLine("facade 4.5: "); }
+      if (shortest == facade46_msg) { Console.WriteLine("facade 4.6: "); }
+      foreach (var m in shortest)
+      {
+        Console.WriteLine(m);
+      }
+      */
+      why = shortest;
+      //Debug.Assert(false, "Couldn't fix project: " + proj.FilePath);
+      return ReferenceStatus.Broken;
+    }
+    static bool HasErrors(Project proj, out List<string> why)
+    {
+      why = new List<string>();
+      try
+      {
+        var cu = proj.GetCompilationAsync().Result;
+        foreach (var e in cu.GetDiagnostics().Where(x => x.Severity == DiagnosticSeverity.Error))
+        {
+          why.Add(String.Format("{0}: {1}", e.Location, e.GetMessage()));
+        }
+        return why.Count() != 0;
+      }
+      catch (AggregateException e)
+      {
+        foreach (var ie in e.InnerExceptions)
+        {
+          why.Add(ie.Message);
+        }
+        return false;
+
+      }
+      catch (Exception e)
+      {
+        why.Add(e.Message);
+        return false;
+      }
+    }
 
     static string stripBaseDir(string otherdir)
     {
